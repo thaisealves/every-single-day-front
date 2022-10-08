8caeb4c38708c0c18b1895f49d5bc3eeeaec8c12
thaisealves <alves.thaisesilva@gmail.com>
2022-10-08 02:35:01 -0300

    feat: adding the add button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,38 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import PageContext from "../PageContext";
-
+import { SpeedDial, SpeedDialIcon, SpeedDialAction } from "@mui/material";
+import { HiOutlinePhotograph } from "react-icons/hi";
+import styled from "styled-components";
 export default function Home() {
   const { setPageName } = useContext(PageContext);
-  // setPageName("Vision Board");
-  return <div>Home</div>;
+  useEffect(() => {
+    setPageName("Vision Board");
+  }, []);
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Container>
+      <SpeedDial
+        ariaLabel="Adding a vision"
+        sx={{ position: "absolute", bottom: 80, right: 16 }}
+        icon={<SpeedDialIcon />}
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        open={open}
+      >
+        <SpeedDialAction
+          icon={<HiOutlinePhotograph />}
+          tooltipTitle={"Vision"}
+          tooltipOpen
+        />
+      </SpeedDial>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  margin: 59px 0;
+  .MuiSpeedDial-root button{
+  background-color: #7C9982;
+  }
+`;
